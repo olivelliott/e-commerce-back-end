e725c4e6fe1d48ee43194d14dbfe07c0548665f7
Olive <olivelliott48@gmail.com>
2022-07-10 21:41:54 -0400

    Create Tag, ProductTag Models, set up initial relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
